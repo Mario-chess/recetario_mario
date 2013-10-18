640f90c192903af8c1606a6e3bff7d91d8cce60d
Mario-chess <mario.a_555@hotmail.es>
2013-10-18 17:39:28 +0000

    Resolviendo

--- manage.py
+++ manage.py
@@ -3,11 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recetario_mario.settings")
-=======
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recetario.settings")
->>>>>>> ef91fd9b0446cc8dca721399711ac6ef81ee7678
 
     from django.core.management import execute_from_command_line
 
